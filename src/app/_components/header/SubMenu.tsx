34daf020c77232b9c3696e74f8c9b6afd62d3fcf
Zephinax <zephinax@gmail.com>
2025-01-27 18:00:23 -0800

    change logo, fix menu styles and add click outside close

--- src/app/_components/header/SubMenu.tsx
+++ src/app/_components/header/SubMenu.tsx
@@ -1,10 +1,10 @@
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
-import React, { useState } from "react";
-import FlexBetween from "../flex/FlexBetween";
-import Paragraph from "../typography/Paragraph";
-import { Sizes } from "@/_utiles/enums";
-import { NavItemType } from "./types";
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import FlexBetween from '../flex/FlexBetween';
+import Paragraph from '../typography/Paragraph';
+import { ColorType, Sizes } from '@/_utiles/enums';
+import { NavItemType } from './types';
 
 const SubMenu = ({ data }: { data: NavItemType }) => {
   const [openSubMenu, setOpenSubMenu] = useState(false);
@@ -12,8 +12,7 @@ const SubMenu = ({ data }: { data: NavItemType }) => {
     <li
       className="flex items-center gap-2 relative cursor-pointer"
       onMouseEnter={() => setOpenSubMenu(true)}
-      onMouseLeave={() => setOpenSubMenu(false)}
-    >
+      onMouseLeave={() => setOpenSubMenu(false)}>
       <p className="flex items-center gap-2">
         {data.text}
         <ChevronDownIcon className="size-4" />
@@ -21,18 +20,21 @@ const SubMenu = ({ data }: { data: NavItemType }) => {
 
       <div
         className={`absolute right-0 top-full opacity-1  transition-all group-hover/categories:opacity-100 group-hover/categories:visible pt-5 z-10 ${
-          !openSubMenu ? " invisible opacity-0" : ""
-        }`}
-      >
-        <ul className="flex flex-col relative w-56 max-h-[300px] bg-background border border-secondary-500 shadow-2xl shadow-black/5">
+          !openSubMenu ? ' invisible opacity-0' : ''
+        }`}>
+        <ul className="flex flex-col gap-2 p-4 relative w-56 [&>li>a]:!cursor-pointer rounded-xl max-h-[300px] bg-background border border-secondary-500 shadow-2xl shadow-black/5">
           {data.childrens?.map((item, index) => (
             <li className="group" key={index}>
               <Link
                 href={item.path}
-                className=" w-full flex items-center relative transition-colors cursor-pointer  hover:[&>div>p]:!text-primary py-2 px-3"
-              >
+                className=" w-full flex items-center relative transition-colors cursor-pointer py-1 hover:[&>div>p]:!text-primary">
                 <FlexBetween className="w-full">
-                  <Paragraph size={Sizes.sm}>{item.text}</Paragraph>
+                  <Paragraph
+                    size={Sizes.xs}
+                    type={ColorType.BLACK}
+                    className="font-medium">
+                    {item.text}
+                  </Paragraph>
                   {/* <ChevronLeftIcon className="size-4 text-secondary-700" /> */}
                 </FlexBetween>
               </Link>
