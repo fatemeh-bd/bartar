358812d2c954de147782aaaa12dc525983a62c0c
Zephinax <zephinax@gmail.com>
2025-01-27 18:00:23 -0800

    change logo, fix menu styles and add click outside close

--- src/app/_components/flex/FlexItemCenter.tsx
+++ src/app/_components/flex/FlexItemCenter.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import { FlexComponentsType } from "./types";
+import React, { forwardRef } from 'react';
+import { FlexComponentsType } from './types';
 
-const FlexItemCenter = ({
-  children,
-  gap,
-  className,
-  onClick,
-}: FlexComponentsType) => {
-  return (
-    <div
-      className={`flex items-center ${gap ? gap : "gap-2"} ${
-        className ?? className
-      }`}
-      onClick={onClick}
-    >
-      {children}
-    </div>
-  );
-};
+const FlexItemCenter = forwardRef<HTMLDivElement, FlexComponentsType>(
+  ({ children, gap, className, onClick }, ref) => {
+    return (
+      <div
+        ref={ref} // Forward the ref to the div
+        className={`flex items-center ${gap ? gap : 'gap-2'} ${
+          className ?? className
+        }`}
+        onClick={onClick}>
+        {children}
+      </div>
+    );
+  }
+);
+
+FlexItemCenter.displayName = 'FlexItemCenter'; // Optional: Set display name for debugging
 
 export default FlexItemCenter;
